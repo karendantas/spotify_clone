7bb8be673a225128fd303f50b1cfff35101b7933
Karen Dantas <karennobre74@gmail.com>
2024-08-05 20:57:58 -0300

    feat: create home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
+import { House, MagnifyingGlass } from "phosphor-react"
 
 function App() {
 
 
   return (
-   <>
-   <h1 className="text-pink-600">ola, mundo</h1>
-   </>
+    <main className="flex flex-col w-full h-screen space-y-2 p-2">
+
+      <div className="flex h-full space-x-2">
+        <section className="flex flex-col gap-2 w-1/3 h-full">
+            <div className="bg-spotify-gray-800 w-full h-[13%] rounded-md flex flex-col gap-5 justify-center pl-5">
+              <div className="flex items-center gap-4 text-spotify-gray-300">
+                <House className="size-7 " />
+                <span className="font-bold">Início</span>
+              </div>
+
+              <div className="flex items-center gap-4 text-spotify-gray-300">
+                <MagnifyingGlass className="size-7 " />
+                <span className="font-bold">Buscar</span>
+              </div>
+
+            </div>
+
+            <div className="bg-spotify-gray-800 w-full h-[87%] rounded-md">
+
+            </div>
+        </section>
+        <section className="bg-gradient-to-t from-spotify-gray-800 to-spotify-gray-500 w-full h-full rounded-md"> </section>
+
+      </div>
+      <section className="bg-black w-full h-24"> </section>
+    </main>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{ts,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        sgreen: '#1DB954',
+        'spotify-gray-800': '#121212',
+        'spotify-gray-500': '#202020',
+        'spotify-gray-400': '#656565',
+        'spotify-gray-300': '#b3b3b3',
+        'spotify-gray-200': '#F9F9F9',
+      }
+    },
   },
   plugins: [],
 }
